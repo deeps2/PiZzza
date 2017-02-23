56aaa1a47edf19bb66896c926303e2e109bc5533
SHIKHAR DEEP <deepshikhar24@yahoo.com>
2017-02-23 19:05:46 +0530

    implemented dynamic radio groups and radio buttons

--- app/src/main/java/com/shikhar/pizzza/MainActivity.java
+++ app/src/main/java/com/shikhar/pizzza/MainActivity.java
@@ -2,7 +2,18 @@ package com.shikhar.pizzza;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
 import com.shikhar.pizzza.model.PizzaResponse;
+import com.shikhar.pizzza.model.VariantGroup;
+import com.shikhar.pizzza.model.Variation;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -15,10 +26,21 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String BASE_URL = "https://api.myjson.com/";
 
+    List<VariantGroup> mlistVariantGroup = new ArrayList<>();
+    List<Variation> mlistVariations = new ArrayList<>();
+
+    LinearLayout mLinearLayout;
+
+    RadioGroup rg;
+    RadioButton[] rb;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLinearLayout = (LinearLayout) findViewById(R.id.activity_main);
 
         callWebService();
     }
@@ -43,12 +65,45 @@ public class MainActivity extends AppCompatActivity {
         call.enqueue(new Callback<PizzaResponse>() {
             @Override
             public void onResponse(Call<PizzaResponse> call, Response<PizzaResponse> response) {
-                
+
+                int status = response.code();
+
+                if (status != 200) {
+                    Log.d("Status code NOT Ok. ", "Status Code: " + status);
+                    return ;
+                }
+
+                mlistVariantGroup = response.body().getVariants().getVariantGroups();
+
+                for (int i = 0 ; i < mlistVariantGroup.size(); i++){
+
+                    rg = new RadioGroup(MainActivity.this);
+
+                   // rg.setId(Integer.parseInt(mlistVariantGroup.get(i).getGroupId()));
+                    //rg.setTag(mlistVariantGroup.get(i).getName());
+                    rg.setOrientation(RadioGroup.HORIZONTAL);
+
+                    mlistVariations = mlistVariantGroup.get(i).getVariations();
+
+                    rb = new RadioButton[mlistVariations.size()];
+                    for(int j = 0; j < mlistVariations.size(); j++){
+
+
+
+                        rb[j] = new RadioButton(MainActivity.this);
+                        rb[j].setText(mlistVariations.get(j).getName());
+
+                        rg.addView(rb[j]);
+                    }
+
+                    mLinearLayout.addView(rg);
+                   // rg.setId(Integer.parseInt(mlistVariantGroup.get(i).getGroupId()));
+                }
             }
 
             @Override
             public void onFailure(Call<PizzaResponse> call, Throwable t) {
-
+                Log.d("onFailure", t.toString());
             }
         });
     }
